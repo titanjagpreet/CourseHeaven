a9b82d742e57afd34f7a0dc47343ea21dd8f3f73
titanjagpreet <titanjagpreetsingh@gmail.com>
2025-05-03 08:40:37 +0530

    Admin & User middleware for Route protection

---
+++ middlewares/userMw.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const JWT_USER_SECRET = process.env.JWT_USER_SECRET;
+
+const userMiddleware = (req, res, next) => {
+    
+    const token = req.headers.token;
+    const decoded = jwt.verify(token, JWT_USER_SECRET);
+
+    if(decoded){
+        req.userId = decoded.id;
+        next();
+    } else{
+
+        return res.status(403).json({
+            message: "You're not signed in!"
+        });
+    }
+}
+
+module.exports = {
+    userMiddleware,
+}
